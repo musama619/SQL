487bb9e0feb6ddc90ed51022d492d55d02669b32
musama619 <34093998+musama619@users.noreply.github.com>
2020-07-17 17:03:52 +0500

    Update WHERE.sql

--- WHERE/WHERE.sql
+++ WHERE/WHERE.sql
@@ -2,19 +2,20 @@ select * from [P12-OrderBreakdown]
 where Category = 'Furniture'
 and Sales > 100  
 
-#Convert function if data type is not int 
+--Convert function if data type is not int 
 
 select * from [P12-OrderBreakdown]
 where Category = 'Furniture'
 and Convert(FLOAT, Sales )> 100  
 
-#everything in Furniture above 100 Sale is selected and all Technology rows regardless of Sale
+--Everything in Furniture above 100 Sale is selected and all Technology rows regardless of Sale
+
 select * from [P12-OrderBreakdown]
 where Category = 'Furniture' 
 and Convert(FLOAT, Sales )> 100  
 or Category = 'Technology'
 
-#everything in Furniture and Technology above 100 Sale is selected
+--Everything in Furniture and Technology above 100 Sale is selected
 
 select * from [P12-OrderBreakdown]
 where (Category = 'Furniture' 
