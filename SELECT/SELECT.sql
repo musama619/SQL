61121645df0b4fc326f27d0b60d4201858ba8c82
musama619 <34093998+musama619@users.noreply.github.com>
2020-07-17 16:24:45 +0500

    Update SELECT.sql

--- SELECT/SELECT.sql
+++ SELECT/SELECT.sql
@@ -1,9 +1,14 @@
+### Show top 1000 rows
+
 SELECT TOP 1000 [Order ID]
       ,[Order Date]
       ,[Customer Name]
       ,[Country]
   FROM [DS].[dbo].[P12-ListOfOrders]
+--------------------------------------
+### Show all rows
 
 SELECT * 
   FROM [DS].[dbo].[P12-ListOfOrders]
 
+SELECT * FROM [P12-ListofOrders]
