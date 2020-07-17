76548eccdcd6ad8d32050aa035b025ef643548b9
musama619 <34093998+musama619@users.noreply.github.com>
2020-07-17 17:34:34 +0500

    Update cast_convert.sql

--- Explicit Type Conversion/cast_convert.sql
+++ Explicit Type Conversion/cast_convert.sql
@@ -11,4 +11,4 @@ FROM [P12-OrderBreakdown]
 ORDER BY CONVERT(FLOAT, [Sales]) DESC
 
 
-CAST vs CONVERT: https://www.essentialsql.com/what-is-the-difference-between-cast-and-convert/#:~:text=CAST%20is%20part%20of%20the,that%20is%20used%20for%20formatting.
+-- CAST vs CONVERT: https://www.essentialsql.com/what-is-the-difference-between-cast-and-convert/#:~:text=CAST%20is%20part%20of%20the,that%20is%20used%20for%20formatting.
